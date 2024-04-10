7830787f4a97e9af22d00887396b9ba34b1ec511
suraj Kumar <surajkumar71799@gmail.com>
2024-04-11 02:38:16 +0530

    done here

--- server/dj1/djapp/views.py
+++ server/dj1/djapp/views.py
@@ -1,5 +1,4 @@
 from rest_framework.decorators import api_view
-import requests
 from .models import Users
 from rest_framework.response import Response
 from .serielizer import UserSerializer , PostSerializer
